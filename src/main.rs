3327f03a8c9f153e7786418f2c656b82fe44597f
AbleArcher88 <mmcso1000@yahoo.com>
2022-10-31 10:33:29 -0500

    added self test feature

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,11 @@ use pac::interrupt;
 use hal::{clock::*, flash::*, gpio::*, pcm::*, timer::*, watchdog::*};
 use embedded_hal::digital::PinState;
 
-const DEV_MAX: u8 = 1; // AND = 0, OR = 1
+// Truth Tables
+const AND_TABLE: [bool; 4] = [false, false, false, true];
+const OR_TABLE: [bool; 4] = [false, true, true, true];
+
+const DEV_MAX: u8 = 2; // AND = 0, OR = 1, self test = 2
 const MODE_MAX: u8 = 4; // 0 = test all, 1-4 = test specific gate
 
 static CFG_BUTTON_1: Mutex<RefCell<Option<hal::gpio::porta::P1_1<Input<PullUp>>>>> = Mutex::new(RefCell::new(None)); // The right push button, to switch the device under test
@@ -34,7 +38,7 @@ static GATE1_IN2: Mutex<RefCell<Option<hal::gpio::portc::P5_1<Output>>>> = Mutex
 static GATE1_OUT1: Mutex<RefCell<Option<hal::gpio::portc::P5_2<Input<PullUp>>>>> = Mutex::new(RefCell::new(None)); // Gate 1 Output 1
 
 // Gate 2 test peripheals
-static GATE2_IN1: Mutex<RefCell<Option<hal::gpio::portc::P5_4<Output>>>> = Mutex::new(RefCell::new(None));
+static GATE2_IN1: Mutex<RefCell<Option<hal::gpio::portc::P5_4<Output>>>> = Mutex::new(RefCell::new(None)); // Diddo
 static GATE2_IN2: Mutex<RefCell<Option<hal::gpio::portc::P5_5<Output>>>> = Mutex::new(RefCell::new(None));
 static GATE2_OUT1: Mutex<RefCell<Option<hal::gpio::portc::P5_6<Input<PullUp>>>>> = Mutex::new(RefCell::new(None));
 
@@ -48,6 +52,32 @@ static GATE4_IN1: Mutex<RefCell<Option<hal::gpio::portc::P6_5<Output>>>> = Mutex
 static GATE4_IN2: Mutex<RefCell<Option<hal::gpio::portc::P6_6<Output>>>> = Mutex::new(RefCell::new(None));
 static GATE4_OUT1: Mutex<RefCell<Option<hal::gpio::portc::P6_7<Input<PullUp>>>>> = Mutex::new(RefCell::new(None));
 
+
+fn self_test() -> [bool; 4] {
+    let mut returntable: [bool; 4] = [false, false, false, false];
+    cortex_m::interrupt::free(|cs| {
+        let mut pin1 = GATE1_IN1.borrow(cs).borrow_mut();
+        let mut pin2 = GATE1_IN2.borrow(cs).borrow_mut();
+        let pout1 = GATE1_OUT1.borrow(cs).borrow();
+        // 00 test
+        pin1.as_mut().unwrap().set_low();
+        pin2.as_mut().unwrap().set_low();
+        returntable[0] = pout1.as_ref().unwrap().is_high().unwrap();
+        // 01 test
+        pin1.as_mut().unwrap().set_high();
+        pin2.as_mut().unwrap().set_low();
+        returntable[1] = pout1.as_ref().unwrap().is_high().unwrap();
+        // 10 test
+        pin1.as_mut().unwrap().set_low();
+        pin2.as_mut().unwrap().set_high();
+        returntable[2] = pout1.as_ref().unwrap().is_high().unwrap();
+        // 11 test
+        pin1.as_mut().unwrap().set_high();
+        pin2.as_mut().unwrap().set_high();
+        returntable[3] = pout1.as_ref().unwrap().is_high().unwrap();
+    });
+    return returntable;
+}
 fn test_1 (truthtable: &[bool; 4]) -> bool {
     let mut outtable: [bool; 4] = [false, false, false, false];
     cortex_m::interrupt::free(|cs| { // A Crtical Section
@@ -271,44 +301,57 @@ fn main() -> ! {
     bled.set_low();
 
     loop {
-        let dev_table: [bool; 4];
+        let mut dev_table: [bool; 4] = [false, false, false, false];
         if DEV_IND.load(Ordering::Relaxed) == 0 {
             bled.set_low();
             rled.set_high();
-            dev_table = [false, true, true, true];
-        } else {
+            dev_table = OR_TABLE;
+        } else if DEV_IND.load(Ordering::Relaxed) == 1 {
             rled.set_low();
             bled.set_high();
-            dev_table = [false, false, false, true];
+            dev_table = AND_TABLE;
+        } else if DEV_IND.load(Ordering::Relaxed) == DEV_MAX {
+            srled.set_low();
+            gled.set_low();
+            dev_table = self_test();
+            rled.set_state(PinState::from(dev_table == OR_TABLE));
+            bled.set_state(PinState::from(dev_table == AND_TABLE));
+            hprintln!("OR Gate: {} | AND Gate: {}", dev_table == OR_TABLE, dev_table == AND_TABLE);
         }
-        let mut testtable: [bool; 5] = [false, false, false, false, false];
-        if MODE_IND.load(Ordering::Relaxed) == 0 {
-            srled.set_high();
-            testtable = test_all(&dev_table);
-            gled.set_state(PinState::from(testtable[0]));
-            hprintln!("Test result {:?}", testtable);
-        } else if MODE_IND.load(Ordering::Relaxed) == 1 {
-            srled.toggle();
-            gled.set_state(PinState::from(test_1(&dev_table)));
-            hprintln!("Test result {}", test_1(&dev_table));
-        } else if MODE_IND.load(Ordering::Relaxed) == 2 {
-            srled.toggle();
-            gled.set_state(PinState::from(test_2(&dev_table)));
-            hprintln!("Test result {}", test_1(&dev_table));
-        } else if MODE_IND.load(Ordering::Relaxed) == 3 {
-            srled.toggle();
-            hprintln!("Test result {}", test_3(&dev_table));
-            gled.set_state(PinState::from(test_1(&dev_table)));
-        } else if MODE_IND.load(Ordering::Relaxed) == 4 {
-            srled.toggle();
-            gled.set_state(PinState::from(test_4(&dev_table)));
-            hprintln!("Test result {}", test_4(&dev_table));
+        if DEV_IND.load(Ordering::Relaxed) != DEV_MAX {
+            let mut testtable: [bool; 5] = [false, false, false, false, false];
+            if MODE_IND.load(Ordering::Relaxed) == 0 {
+                srled.set_high();
+                testtable = test_all(&dev_table);
+                gled.set_state(PinState::from(testtable[0]));
+                hprintln!("Test result {:?}", testtable);
+            } else if MODE_IND.load(Ordering::Relaxed) == 1 {
+                srled.toggle();
+                gled.set_state(PinState::from(test_1(&dev_table)));
+                testtable[1] = test_1(&dev_table);
+                hprintln!("Test result {}", test_1(&dev_table));
+            } else if MODE_IND.load(Ordering::Relaxed) == 2 {
+                srled.toggle();
+                gled.set_state(PinState::from(test_2(&dev_table)));
+                testtable[2] = test_2(&dev_table);
+                hprintln!("Test result {}", test_1(&dev_table));
+            } else if MODE_IND.load(Ordering::Relaxed) == 3 {
+                srled.toggle();
+                testtable[3] = test_3(&dev_table);
+                hprintln!("Test result {}", test_3(&dev_table));
+                gled.set_state(PinState::from(test_1(&dev_table)));
+            } else if MODE_IND.load(Ordering::Relaxed) == 4 {
+                srled.toggle();
+                gled.set_state(PinState::from(test_4(&dev_table)));
+                testtable[4] = test_4(&dev_table);
+                hprintln!("Test result {}", test_4(&dev_table));
+            }
+            testall_led.set_state(PinState::from(testtable[0]));
+            test1_led.set_state(PinState::from(testtable[1]));
+            test2_led.set_state(PinState::from(testtable[2]));
+            test3_led.set_state(PinState::from(testtable[3]));
+            test4_led.set_state(PinState::from(testtable[4]));
         }
-        testall_led.set_state(PinState::from(testtable[0]));
-        test1_led.set_state(PinState::from(testtable[1]));
-        test2_led.set_state(PinState::from(testtable[2]));
-        test3_led.set_state(PinState::from(testtable[3]));
-        test4_led.set_state(PinState::from(testtable[4]));
         _watchdog.feed().unwrap();
         continue;
     }
